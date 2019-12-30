e94df7107a847b867e18bfe66bd81c39a3833f84
Jan Zobniów <jan.zobniow@gmail.com>
2019-12-30 15:42:03 +0100

    (Male uporzadkowanie)

--- main.py
+++ main.py
@@ -1,65 +1,72 @@
 import pygame
 
+#Screen parameters:
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 class Mob:
-    def __init__(self, name, health, damage): # TO DO
+    def __init__(self, name, velocity, health, scene = None):
         self.name = name
+        self.velocity = 1
         self.health = health
-        self.damage = damage
+        self.scene = scene # Scena w jakiej sie znajduje
 
 class Player(Mob):
     def __init__(self, name, image):
-        Mob.__init__(self, name, 100, 10)
+        Mob.__init__(self, name, 1, 100, 10)
 
         self.image = pygame.image.load(image)
-        self.x = 300
-        self.y = 200
-        self.speed = 0.5
+        self.x = 300 # Ustalam tutaj startowe polozenie
+        self.y = 200 
+        self.width = 64 # Piksele
+        self.height = 64 # Piksele
     
-    def show(self):
-        self.scene.screen.blit(self.image, (self.x, self.y))
+    def show(self): # TO DO: Przerzucic do klasy Mob
+        Game.screen.blit(self.image, (self.x, self.y))
+    
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_LEFT] : self.moveLeft()
+        if keys[pygame.K_RIGHT] : self.moveRight()
 
-    def changeX(self, value):
-        self.x += self.speed * value
+    def moveLeft(self):
+        if self.x > self.velocity:
+            self.x -= self.velocity
 
-    def changeY(self, value):
-        self.y += self.speed * value
+    def moveRight(self):
+        if self.x + self.width < Game.width:
+            self.x += self.velocity
 
-class Game:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.running = True
-        self.screen = pygame.display.set_mode((self.width, self.height)) # Tworzy okienko
-        pygame.display.set_caption('Giereczka') # Ustawia nazwe okienka
-
-    player = Player('Tomek', 'Resources/player.png') # Tworzenie gracza
+class Game: # Wszystkie zmienne gry
+    width = SCREEN_WIDTH
+    height = SCREEN_HEIGHT
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Tworzy okienko
+    pygame.display.set_caption('Giereczka') # Ustawia nazwe okienka
+    player = Player('Tomek', 'Resources/player.png')
         
 
-class Scene(Game):
-    def __init__(self, name, width, height):
-        super().__init__(width, height)
+class Scene:
+    def __init__(self, name):
         self.name = name
+        self.running = True
 
     def on_init(self):
         pygame.init()
-        Game.player.scene = self
+        Game.player.scene = self # Player's scene
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self.running = False
 
+    def on_loop(self): # Wszystkie movementy i inne ify
+        Game.player.movement()
+
     def on_render(self):
-        self.screen.fill((123, 255, 231))
-        self.player.show()
+        Game.screen.fill((123, 255, 231))
+        Game.player.show()
         pygame.display.update()
 
-    def movement(self):
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_LEFT] : Game.player.changeX(-1)
-        if keys[pygame.K_RIGHT] : Game.player.changeX(1)
-        if keys[pygame.K_UP] : Game.player.changeY(-1)
-        if keys[pygame.K_DOWN] : Game.player.changeY(1)
 
     def start(self):
         self.on_init()
@@ -67,11 +74,11 @@ class Scene(Game):
             for event in pygame.event.get():
                 self.on_event(event)
             
-            self.movement()
+            self.on_loop()
             self.on_render()
 
 
 
 if __name__ == "__main__":
-    scena_1 = Scene('Level 1', 800, 600)
+    scena_1 = Scene('Level 1')
     scena_1.start()
